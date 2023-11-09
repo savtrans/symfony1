6e412b28a5a45cc041a95fd09f5c1b012293bc7e
Zachary Lund <git@lundscape.com>
2023-11-09 08:46:33 -0600

    Fix PHP 8.2 deprecations
    - Define dynamically declared class properties.
    - Replace "${var}" string interpolation variables with "{$var}".

--- lib/routing/sfRoute.class.php
+++ lib/routing/sfRoute.class.php
@@ -34,7 +34,9 @@ class sfRoute
     $defaults          = array(),
     $requirements      = array(),
     $tokens            = array(),
-    $customToken       = false;
+    $customToken       = false,
+    $firstOptional     = null,
+    $segments          = array();
 
   /**
    * Constructor.
